1ca7a0753a6b3b06ffe0b903e5ee1999cba19b98
Nathan9889 <nathansayag@gmail.com>
2022-03-20 23:04:59 +0200

    Pr03 commit

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -54,7 +54,7 @@ public class Polygon implements Geometry {
 		if (vertices.length == 3)
 			return; // no need for more tests for a Triangle
 
-		Vector n = plane.getNormal();
+		Vector n = plane.getNormal( );
 
 		// Subtracting any subsequent points will throw an IllegalArgumentException
 		// because of Zero Vector if they are in the same point
