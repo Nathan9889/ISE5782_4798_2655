d678112d8f9cd78fa451fb252cef702bc78248f0
Nathan9889 <nathansayag@gmail.com>
2022-06-06 15:06:05 +0300

    test for mini1

--- src/unittests/renderer/TestForEx7.java
+++ src/unittests/renderer/TestForEx7.java
@@ -1,5 +1,6 @@
 package unittests.renderer;
 
+import geometries.Polygon;
 import geometries.Sphere;
 import geometries.Triangle;
 import lighting.AmbientLight;
@@ -11,42 +12,148 @@ import renderer.ImageWriter;
 import renderer.RayTracerBasic;
 import scene.Scene;
 
+import static java.awt.Color.*;
+
 public class TestForEx7 {
     private Scene scene = new Scene("Test scene");
 
     /**
-     * Produce a picture of a sphere lighted by a spot light
+     * Produce a picture of 3 spheres on a triangle mirror
      */
     @Test
-    public void twoSpheresOnMirrors() {
-        Camera camera = new Camera(new Point(0, 0, 10000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-                .setVPSize(2500, 2500).setVPDistance(10000); //
+    public void allFeature(){
+        Camera camera = new Camera(new Point(0, 0, 5000),
+                new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setVPSize(200, 200).setVPDistance(1000);
 
-        scene.setAmbientLight(new AmbientLight(new Color(255, 255, 255), new Double3(0.1)));
+        Scene scen = new Scene("Test scene")
+                .setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)))
+                .setBackground(new Color(94,97,99));
 
-        scene.geometries.add( //
-                new Sphere(new Point(-950, -900, -1000), 400d).setEmission(new Color(0, 0, 100)) //
-                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0.5)),
-                new Sphere(new Point(-950, -900, -1000), 200d).setEmission(new Color(100, 20, 20)) //
-                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20)),
-                new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500), new Point(670, 670, 3000)) //
-                        .setEmission(new Color(20, 20, 20)) //
-                        .setMaterial(new Material().setKr(1)),
-                new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500),
-                        new Point(-1500, -1500, -2000)) //
-                        .setEmission(new Color(20, 20, 20)) //
-                        .setMaterial(new Material().setKr(0.5)));
+        scen.geometries.add( //
+                new Triangle(new Point(450, -350, 0),
+                        new Point(-500, -400, 0), new Point(0, 500, -80)) //
+                        .setEmission(new Color(75,75,75)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),//
+                new Sphere(new Point(-100, 100, 300), 100d).setEmission(new Color(BLUE)) //
+                        .setMaterial(new Material().setKd(0.3).setKs(0.2).setShininess(30).setKt(0.6)),//
+                new Sphere(new Point(100, -200, 400), 100).setEmission(new Color(RED)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(10).setKt(0.8)),//
+                new Sphere(new Point(200, 100, 800), 50d).setEmission(new Color(GREEN)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(20).setKt(0.3)));
 
-        scene.lights.add(new SpotLight(new Color(1020, 400, 400), new Point(-750, -750, -150), new Vector(-1, -1, -4)) //
-                .setKl(0.00001).setKq(0.000005));
+        scen.lights.add(new SpotLight(new Color(700, 400, 400),
+                new Point(0, 0, 900), new Vector(0.5, 2.5, -7.5)) //
+                .setKl(4E-5).setKq(2E-7));
 
-        ImageWriter imageWriter = new ImageWriter("TestForEx7", 500, 500);
+        ImageWriter imageWriter = new ImageWriter("TestForEx7", 600, 600);
         camera.setImageWriter(imageWriter) //
-                .setRayTracer(new RayTracerBasic(scene)) //
+                .setRayTracer(new RayTracerBasic(scen)) //
                 .renderImage() //
                 .writeToImage();
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Produce a picture of 3 spheres on a triangle mirror
+     */
+    @Test
+    public void Xylo(){
+        Camera camera = new Camera(new Point(-6000, -6000, 3200),
+                new Vector(1, 1, -1d/2), new Vector(1, 1, 4)) //
+                .setVPSize(200, 200).setVPDistance(2000);
+
+        Scene scen = new Scene("Test scene")
+                .setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)))
+                .setBackground(new Color(94,97,99));
+
+        scen.geometries.add( //
+                new Polygon(
+                        new Point(-300,200,75),
+                        new Point(-300,-200,75),
+                        new Point(400,-140,75),
+                        new Point(400,140,75))//
+                        .setEmission(new Color(75,75,75)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),//
+
+
+                new Polygon(
+                        new Point(-300,200,-75),
+                        new Point(-300,-200,-75),
+                        new Point(400,-140,-75),
+                        new Point(400,140,-75))//
+                        .setEmission(new Color(75,75,75)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+
+                new Polygon(
+                        new Point(-300,200,-75),
+                        new Point(-300,-200,-75),
+                        new Point(-300,-200,75),
+                        new Point(-300,200,75))//
+                        .setEmission(new Color(75,75,75)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+                new Polygon(
+                        new Point(-300,-200,75),
+                        new Point(-300,-200,-75),
+                        new Point(400,-140,-75),
+                        new Point(400,-140,75))//
+                        .setEmission(new Color(75,75,75)) //
+                        .setMaterial(new Material().setKr(1).setKs(0.1).setShininess(10)),
+
+
+                new Sphere(new Point(-100, 100, 300), 100d).setEmission(new Color(BLUE)) //
+                        .setMaterial(new Material().setKd(0.3).setKs(0.2).setShininess(30).setKt(0.6)),//
+                new Sphere(new Point(100, -200, 400), 100).setEmission(new Color(RED)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(10).setKt(0.8)),//
+                new Sphere(new Point(200, 100, 800), 50d).setEmission(new Color(GREEN)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(20).setKt(0.3)));
+
+        scen.lights.add(new SpotLight(new Color(700, 400, 400),
+                new Point(0, 0, 1200), new Vector(0.5, 2.5, -7.5)) //
+                .setKl(4E-5).setKq(2E-7));
+
+        ImageWriter imageWriter = new ImageWriter("Xylo", 600, 600);
+        camera.setImageWriter(imageWriter) //
+                .setRayTracer(new RayTracerBasic(scen)) //
+                .renderImage() //
+                .writeToImage();
+    }
 
 
 }
